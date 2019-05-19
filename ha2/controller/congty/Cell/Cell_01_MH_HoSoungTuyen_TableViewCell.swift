8dc253ad4a48843cd27876c8db09b54b968dbd94
vu minh tame <vuminhtam.sdb@gmail.com>
2019-05-19 23:39:07 +0700

    luac 11:38 ngu 1

---
+++ ha2/controller/congty/Cell/Cell_01_MH_HoSoungTuyen_TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  Cell_01_MH_HoSoungTuyen_TableViewCell.swift
+//  ha2
+//
+//  Created by vuminhtam on 5/19/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class Cell_01_MH_HoSoungTuyen_TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ha2/controller/congty/Cell/Cell_MH_HoSoUngTuyen_TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  Cell_MH_HoSoUngTuyen_TableViewCell.swift
+//  ha2
+//
+//  Created by vuminhtam on 5/19/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class Cell_MH_HoSoUngTuyen_TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
